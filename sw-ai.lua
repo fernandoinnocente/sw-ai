f4afce7b580c79c41606008cb4361ffe5dacad7d
Fernando Cesar Domingues Innocente <fcesar@MACCIT012416.local>
2017-08-29 09:16:06 -0300

    Removendo similaridade especifica

--- sw-ai.lua
+++ sw-ai.lua
@@ -12,7 +12,7 @@ dialogShow("Configurações")
 -- ==========  patterns ===========
 
 startButton = Pattern("flash.png")
-victoryDiamond = Pattern("victoryDiamond.png"):similar(0.8)
+victoryDiamond = Pattern("victoryDiamond.png")
 repeatButton = Pattern("smallFlash.png")
 buttonLeft = Pattern("buttonLeft.png")
 buttonRight = Pattern("buttonRight.png")
