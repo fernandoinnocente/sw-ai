f2c5c90b7a4d37e5eaaf2326329fc428b1c99c9e
fernandoinnocente <fernando.innocente@hotmail.com>
2019-05-10 11:05:36 -0300

    Ajustes programa riftResult

--- sw-ai.lua
+++ sw-ai.lua
@@ -23,7 +23,7 @@ dialogShow("Configurações")
 
 startButton = Pattern("flash.png")
 victoryDiamond = Pattern("victoryDiamond.png"):similar(0.8)
-riftResult = Pattern("riftResult.png"):similar(0.8)
+riftResult = Pattern("riftResult.png"):similar(0.7)
 repeatButton = Pattern("smallFlash.png")
 sellButton = Pattern("sell.en.png")
 getButton = Pattern("get.en.png")
